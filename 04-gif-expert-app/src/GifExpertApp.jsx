755a071df7d2fe140428af284ffadb45ea4c2beb
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-04-23 12:01:15 -0300

    feat: add global styles

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,13 @@
 const GifExpertApp = () => {
   return (
     <>
+      {/* Titulo */}
       <h1>GifExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de Gif */}
+      {/* Gif Item */}
     </>
   );
 };
