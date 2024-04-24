2b36424e764cd0c5d808699839aa9a27d521851f
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-04-24 14:52:53 -0300

    fix: update to the api request

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,17 +1,14 @@
+import { useEffect } from "react";
+import { getGifs } from "../helpers/getGifs";
+
 const GifGrid = ({ category }) => {
-  const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=2wrNbPss1oZsMXIXvlz1iuW3aB4D0c2d&q=${category}&limit=20`;
+  useEffect(() => {
+    getGifs(category);
+  }, []);
 
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-    console.log(data);
-  };
-
-  getGifs();
   return (
     <>
       <h3>{category}</h3>
-      <p>Hola Mundo!</p>
     </>
   );
 };
